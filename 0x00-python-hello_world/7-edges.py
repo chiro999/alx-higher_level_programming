cb0a10a2a791d1d89e25f99b5224cefb47695601
chiro999 <r.o.g.u.e@outlook.com>
2023-09-04 01:35:58 -0700

    7

---
+++ 0x00-python-hello_world/7-edges.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+word = "Holberton"
+first_3 = word[:3]
+last_2 = word[-2:]
+middle = word[1:-1]
+print("First 3 letters: {}".format(first_3))
+print("Last 2 letters: {}".format(last_2))
+print("Middle word: {}".format(middle))
