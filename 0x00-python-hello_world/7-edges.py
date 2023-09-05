7dcd235bf268661a1bc318775ed5c9a0ee7ad580
chiro999 <r.o.g.u.e@outlook.com>
2023-09-04 22:44:56 -0700

    7

--- 0x00-python-hello_world/7-edges.py
+++ 0x00-python-hello_world/7-edges.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 word = "Holberton"
-first_3 = word[:3]
-last_2 = word[-2:]
-middle = word[1:-1]
-print("First 3 letters: {}".format(first_3))
-print("Last 2 letters: {}".format(last_2))
-print("Middle word: {}".format(middle))
+word_first_3 = word[:3]
+word_last_2 = word[-2:]
+middle_word = word[1:-1]
+print("First 3 letters: {}".formato(word_first_3))
+print("Last 2 letters: {}".format(word_last_2))
+print("Middle word: {}".format(middle_word))
