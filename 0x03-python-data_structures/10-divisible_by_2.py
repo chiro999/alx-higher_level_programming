05e1459b8a98c3ff8e72d00061cac38922462582
chiro999 <r.o.g.u.e@outlook.com>
2023-09-09 12:57:19 -0700

    5

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -5,5 +5,5 @@ def divisible_by_2(my_list[]):
         if my_list[i] % 2 == 0:
             new_list.append(True)
         else:
-            new_list.append(Falese)
+            new_list.append(False)
     return new_list
--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    new_string = my_string.translate({ord(l): None for i in 'cC'})
+    new_string = my_string.translate({ord(l): None for l in 'cC'})
     return new_string
