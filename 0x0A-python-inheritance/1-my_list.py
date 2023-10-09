80d9ef66f668e6eb556e00896db39579778d787f
chiro999 <r.o.g.u.e@outlook.com>
2023-10-08 23:50:02 -0700

    done

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" class MyList that inherits from list
+"""
+
+
+
+class MyList(list):
+    """inherits from list"""
+    def print_sorted(self):
+        """prints the list, but sorted
+        (ascending sort)
+        """
+        print(sorted(self))
