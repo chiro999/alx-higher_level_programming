9b88d4b8f210324e95eb9a5afbeb72299964fdfa
Nana Kwame <105567071+chiro999@users.noreply.github.com>
2023-11-15 07:13:57 +0000

    Update 8-count_89.sql

--- 0x0D-SQL_introduction/8-count_89.sql
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -1,4 +1,2 @@
--- Display the number of records with id = 89 in the table first_table.
-SELECT COUNT(*)
-FROM 'first_table'
-WHERE 'id' = 89;
+-- shows records with a particular id
+SELECT COUNT(id) FROM first_table WHERE id = '89'
